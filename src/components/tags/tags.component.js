64535e180732a0517c5276754e364d36377512e1
Chris Esplin <chris@chrisesplin.com>
2018-01-09 14:06:02 +0000

    Refactoring tags.component

--- src/components/tags/tags.component.js
+++ src/components/tags/tags.component.js
@@ -1,9 +1,9 @@
-import { Component } from 'preact';
 import style from './tags.scss';
-import { connect } from 'unistore';
-import { route } from 'preact-router';
-import { store, actions, mappedActions } from '../../datastore';
 import linkState from 'linkstate';
+import { Component } from 'preact';
+import { route } from 'preact-router';
+import { connect } from 'unistore';
+import { store, actions, mappedActions } from '../../datastore';
 import { updateTagsQuery } from '../../queries';
 
 const { updateImage } = mappedActions;
@@ -21,15 +21,9 @@ export default connect('environment,images,selection', actions)(
         route('/images');
       }, 1000);
     } else {
-      const selectedImages = images.filter(image => selection.has(image.__id));
-      const items = selectedImages.map(getItem({ environment }));
-      const tags = selectedImages.reduce((tags, image) => {
-        if (image.tags) {
-          image.tags.forEach(tag => tags.add(tag));
-        }
-        return tags;
-      }, new Set());
-      const globalTagItems = Array.from(tags).map(getGlobalTagItem({ environment, images }));
+      const selectedImages = getSelectedImages({ images, selection });
+      const items = getItems({ environment, selectedImages });
+      const globalTagItems = getGlobalTagItems({ environment, images, selectedImages });
       return (
         <div>
           <form class={style.form} onSubmit={handleSubmit({ environment, images, selection })}>
@@ -57,6 +51,74 @@ export default connect('environment,images,selection', actions)(
   }
 );
 
+// Extract data
+function getSelectedImages({ images, selection }) {
+  return images.filter(image => selection.has(image.__id));
+}
+
+function getItems({ environment, selectedImages }) {
+  return selectedImages.map(image => {
+    const version = image.versions.x200;
+    const name = image.name.split('/').pop();
+    const tagItems = image.tags && image.tags.map(getTagItem({ environment, image }));
+
+    return (
+      <li>
+        <div class={style.image} style={`background: url(${version.url})`} />
+        <div class={style.secondary}>
+          <h3>{name}</h3>
+          <ul class={style.tagItems}>{tagItems}</ul>
+        </div>
+      </li>
+    );
+  });
+}
+
+function getGlobalTagItems({ environment, images, selectedImages }) {
+  const tags = getTags(selectedImages);
+  return Array.from(tags).map(getGlobalTagItem({ environment, images }));
+}
+
+function getTags(selectedImages) {
+  return selectedImages.reduce((tags, image) => {
+    if (image.tags) {
+      image.tags.forEach(tag => tags.add(tag));
+    }
+    return tags;
+  }, new Set());
+}
+
+function getGlobalTagItem({ environment, images }) {
+  return tag => {
+    return (
+      <li class={style.tagItem}>
+        <img
+          src="/assets/svg/delete.svg"
+          alt="delete"
+          onClick={handleGlobalTagClick({ environment, images, tag })}
+        />
+        <span>#{tag}</span>
+      </li>
+    );
+  };
+}
+
+function getTagItem({ environment, image }) {
+  return tag => {
+    return (
+      <li class={style.tagItem}>
+        <img
+          src="/assets/svg/delete.svg"
+          alt="delete"
+          onClick={handleTagClick({ environment, image, tag })}
+        />
+        <span>#{tag}</span>
+      </li>
+    );
+  };
+}
+
+// Handle events
 function handleSubmit({ environment, images, selection }) {
   return e => {
     e.preventDefault();
@@ -78,39 +140,6 @@ function handleSubmit({ environment, images, selection }) {
   };
 }
 
-function getItem({ environment }) {
-  return image => {
-    const version = image.versions.x200;
-    const name = image.name.split('/').pop();
-    const tagItems = image.tags && image.tags.map(getTagItem({ environment, image }));
-
-    return (
-      <li>
-        <div class={style.image} style={`background: url(${version.url})`} />
-        <div class={style.secondary}>
-          <h3>{name}</h3>
-          <ul class={style.tagItems}>{tagItems}</ul>
-        </div>
-      </li>
-    );
-  };
-}
-
-function getTagItem({ environment, image }) {
-  return tag => {
-    return (
-      <li class={style.tagItem}>
-        <img
-          src="/assets/svg/delete.svg"
-          alt="delete"
-          onClick={handleTagClick({ environment, image, tag })}
-        />
-        <span>#{tag}</span>
-      </li>
-    );
-  };
-}
-
 function handleTagClick({ environment, image, tag }) {
   return e => {
     const tags = new Set(image.tags || []);
@@ -118,21 +147,6 @@ function handleTagClick({ environment, image, tag }) {
 
     optimisticUpdate({ image, tags });
     updateTagsQuery({ environment, tags, id: image.__id });
-  };
-}
-
-function getGlobalTagItem({ environment, images }) {
-  return tag => {
-    return (
-      <li class={style.tagItem}>
-        <img
-          src="/assets/svg/delete.svg"
-          alt="delete"
-          onClick={handleGlobalTagClick({ environment, images, tag })}
-        />
-        <span>#{tag}</span>
-      </li>
-    );
   };
 }
 
